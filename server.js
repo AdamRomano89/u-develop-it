e0b873aec340e73c4165cf5ab33ca85c8179ca08
Adam Romano <adamromano1989@gmail.com>
2021-10-23 18:59:00 -0500

    Performing CRUD operation using MySql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const express = require('express')
 const mysql = require('mysql2')
 
@@ -22,9 +23,41 @@ const db = mysql.createConnection(
 );
 
 // Register Routes
-db.query(`SELECT * FROM candidates`, (err, rows)=> {
-  console.log(rows);
-})
+
+// db.query(`SELECT * FROM candidates`, (err, rows)=> {
+//   console.log(rows);
+// })
+
+//Select a candidate
+// db.query(`SELECT * FROM candidates WHERE id = 15`, (err, row) => {
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+//Create a candidate
+// const sql = `
+// INSERT INTO candidates (id, first_name, last_name, industry_connected)
+// VALUES (?,?,?,?)
+// `;
+// const params = [1, 'Ronald', 'Firbank', 1]
+// db.query(sql, params, (err, res) => {
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(res);
+// })
+
 
 
 // Register Error Routes: 404
