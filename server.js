8e1d6c9b487a9a8fc2be5241b5a83f88eda7a36b
Adam Romano <adamromano1989@gmail.com>
2021-10-23 21:13:55 -0500

    adding CRUD end points

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
+// ******************************************************
 // Register Routes
 
 // db.query(`SELECT * FROM candidates`, (err, rows)=> {
@@ -57,6 +58,104 @@ const db = mysql.createConnection(
 //   }
 //   console.log(res);
 // })
+//****************************************************** */
+
+
+app.get('/api/candidates', (req, res) => {
+
+  // Get The Data 
+  const sql = `SELECT * FROM candidates`;
+  
+  // Perform The Query
+  db.query(sql, (err, rows) => {
+
+    if(err) {
+      return res.status(500).json({
+        err: err.message
+      })
+    }
+    res.json({
+      message:'Data returned sucessfully',
+      data: rows
+    })
+  })
+
+})
+
+app.get('/api/candidate/:id', (req, res) => {
+  
+// Get the candidate by it id 
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const param = req.params.id;
+  db.query(sql, param, (err, data) => {
+
+    if(err){
+      return res.status(400).json({
+        error:err.message
+      })
+    }
+    //strict equality
+    if(data.length === 0) {
+      return res.json({
+        message:'No results found!'
+      })
+    }
+
+    res.json({
+      message: 'success',
+      data
+    })
+  })
+})
+
+
+//Delete a candidate
+app.delete('/api/candidate/:id',(req, res) => {
+  const sql = `DELETE FROM candidates WHERE id=?`;
+  const params = [req.params.id]
+
+  db.query(sql, params, (err, data) => {
+    if(err){
+      return res.status(400).json(err.message)
+    } 
+    if(!data.affectedRows){
+      return res.json({
+        message:'Canadidate was not found!'
+      })
+    }
+    res.json({
+      message:`You have deleted ${data.affectedRows} candidate!`,
+      changes: data.affectedRows,
+      id: req.params.id
+    })
+  })
+})
+
+
+
+
+// Delete a candidate
+// app.delete('/api/candidate/:id', (req, res) => {
+//   const sql = `DELETE FROM candidates WHERE id = ?`;
+//   const params = [req.params.id];
+
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.statusMessage(400).json({ error: err.message });
+//     } else if (!result.affectedRows) {
+//       res.json({
+//         message: 'Candidate not found'
+//       });
+//     } else {
+//       res.json({
+//         message: 'deleted',
+//         changes: result.affectedRows,
+//         id: req.params.id
+//       });
+//     }
+//   });
+// });
+
 
 
 
