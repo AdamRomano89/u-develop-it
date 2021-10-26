5c4a858e2290845e2674e20dbe9523c242184c19
Adam Romano <adamromano1989@gmail.com>
2021-10-26 13:20:48 -0500

    adding an update end point

--- server.js
+++ server.js
@@ -172,30 +172,80 @@ app.post('/api/candidate', (req, res) => {
   })
 });
 
-// Delete a candidate
-// app.delete('/api/candidate/:id', (req, res) => {
-//   const sql = `DELETE FROM candidates WHERE id = ?`;
-//   const params = [req.params.id];
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, data) => {
+    if (err) {
+      return res.status(500).json({error: err.message});
+    }
+    res.json({
+      message:`sucess`,
+      data
+    })
+  })
+})
 
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.statusMessage(400).json({ error: err.message });
-//     } else if (!result.affectedRows) {
-//       res.json({
-//         message: 'Candidate not found'
-//       });
-//     } else {
-//       res.json({
-//         message: 'deleted',
-//         changes: result.affectedRows,
-//         id: req.params.id
-//       });
-//     }
-//   });
-// });
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Party not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
 
 
+//update a candidte party_id
+app.put('/api/candidate/:id', (req, res) => {
 
+  const {party_id} = req.body;
+  const {id} = req.params;
+
+  const errors = inputCheck(req.body, 'party_id');
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`
+
+  db.query(sql, [party_id, id], (err, data) => {
+    if(err) return res.status(400).json({error: err.message});
+    if(!data.affectedRows) return res.status(404).json({error: "candidate was not found"})
+    res.json({
+      message: `You have updates ${data.affectedRows} candidate`,
+      data
+    })
+  })
+})
 
 // Register Error Routes: 404
 app.use((req,res) =>{
